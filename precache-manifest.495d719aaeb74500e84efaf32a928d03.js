955259535ef2eb5a89e5fae14a437bcde8b0ca89
Joey Gracey <joey@yewno.com>
2019-11-22 20:40:15 -0800

    Updates

---
+++ precache-manifest.495d719aaeb74500e84efaf32a928d03.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2dd4731326d1534abcd32e3d18e4ad2f",
+    "url": "/neighbs-quiz/index.html"
+  },
+  {
+    "revision": "f9df37921aa859e7a4c5",
+    "url": "/neighbs-quiz/static/css/main.dfca195d.chunk.css"
+  },
+  {
+    "revision": "30328f4d0491f76223fc",
+    "url": "/neighbs-quiz/static/js/2.08b4d0d1.chunk.js"
+  },
+  {
+    "revision": "f9df37921aa859e7a4c5",
+    "url": "/neighbs-quiz/static/js/main.490653e9.chunk.js"
+  },
+  {
+    "revision": "2b0f03a6c5841659b5cd",
+    "url": "/neighbs-quiz/static/js/runtime-main.0e4163bc.js"
+  }
+]);
