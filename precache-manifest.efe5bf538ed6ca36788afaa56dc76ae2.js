1ea6659ad36364ed669c68da606981c70b6fd082
Joey Gracey <joey@yewno.com>
2019-11-26 14:32:20 -0800

    Updates

--- precache-manifest.efe5bf538ed6ca36788afaa56dc76ae2.js
+++ precache-manifest.efe5bf538ed6ca36788afaa56dc76ae2.js
@@ -1,19 +1,19 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "766040ae4dc8a4c0420b4cfbc36ba77e",
+    "revision": "6b914cff91969ba62a75c80ab2b0b84d",
     "url": "/neighbs-quiz/index.html"
   },
   {
-    "revision": "116e061ce708e45b7641",
+    "revision": "a04eb67827d1ce6dc5e4",
     "url": "/neighbs-quiz/static/css/main.dfca195d.chunk.css"
   },
   {
-    "revision": "2a64c12ad447d6922c9f",
-    "url": "/neighbs-quiz/static/js/2.28fa14c9.chunk.js"
+    "revision": "13773737bf439579c577",
+    "url": "/neighbs-quiz/static/js/2.a231e2b5.chunk.js"
   },
   {
-    "revision": "116e061ce708e45b7641",
-    "url": "/neighbs-quiz/static/js/main.dd7405c5.chunk.js"
+    "revision": "a04eb67827d1ce6dc5e4",
+    "url": "/neighbs-quiz/static/js/main.dd973a80.chunk.js"
   },
   {
     "revision": "2b0f03a6c5841659b5cd",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/neighbs-quiz/precache-manifest.e5f9b7de5a17be5298a085e91b670f5f.js"
+  "/neighbs-quiz/precache-manifest.efe5bf538ed6ca36788afaa56dc76ae2.js"
 );
 
 self.addEventListener('message', (event) => {
