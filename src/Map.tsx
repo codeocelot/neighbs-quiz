8dfcdda40020f010e6769133bbbb612962992eb3
Joey Gracey <joey@yewno.com>
2019-11-26 14:31:46 -0800

    feat: add tooltip, learning mode
    - add tooltip to label neighbs after they've been identified.
    - add learning mode to help novices.

--- src/Map.tsx
+++ src/Map.tsx
@@ -1,7 +1,7 @@
 import * as d3 from 'd3';
-import { Selection } from 'd3';
+import { Selection, event } from 'd3';
 import { geoMercator, geoPath } from 'd3-geo';
-import React, { useEffect, createRef, useState, useLayoutEffect, useRef, useMemo, useCallback } from 'react';
+import React, { Fragment, useEffect, createRef, useState, useLayoutEffect, useRef, useMemo, useCallback } from 'react';
 import data from './geojson.json';
 import shuffle from './shuffle';
 
@@ -9,18 +9,30 @@ const DEFAULT_FILL = 'rgb(175, 157, 150)';
 const WRONG_FILL = 'red';
 const CORRECT_FILL = 'green';
 
+enum Mode {
+  game,
+  learn,
+}
+
 export default function Map(): JSX.Element {
-  const [missed, setMissed] = useState([]);
+  const [missed, setMissed] = useState<string[]>([]);
   const [neighbToFind, setNeighb] = useState();
   const [allNeighbs, setAllNeighbs] = useState(shuffle(data.features.map((d) => d.properties.name)));
+  const [selected, setSelected] = useState();
+  const [mode, setMode] = useState<Mode>(Mode.game);
   const rootRef = useMemo(() => createRef<HTMLDivElement>(), []);
   let svg = useRef<Selection<SVGSVGElement, unknown, null, undefined>>();
 
+  const tooltipRef = useRef() as React.MutableRefObject<HTMLInputElement>;
 
-  function startGame() {
+  function resetBoard() {
     setAllNeighbs(shuffle(data.features.map((d) => d.properties.name)));
     setMissed([]);
     d3.selectAll('svg path').style("fill", DEFAULT_FILL).style("stroke", "#ffffff");
+  }
+
+  function startGame() {
+    resetBoard();
     nextNeighb();
   }
 
@@ -37,6 +49,63 @@ export default function Map(): JSX.Element {
     setAllNeighbs(allNeighbs.slice(1).concat(neighbToFind));
   }
 
+  const applyMouseover = useCallback((el: Selection<d3.BaseType, {}, null, undefined>, text: string | ((d: any) => string)) => {
+    return el.on("mousemove", function(d: any){
+      if (tooltipRef.current) {
+        tooltipRef.current.style.top = (event.pageY-10)+"px";
+        tooltipRef.current.style.left = (event.pageX+10)+"px";
+        tooltipRef.current.style.visibility = 'visible';
+        tooltipRef.current.innerHTML = typeof text === 'function' ? text(d) : text;
+      }
+    })
+    .on("mouseout", function(){
+      if (tooltipRef.current) {
+        tooltipRef.current.style.visibility = 'hidden';
+      }
+    });
+  },[tooltipRef]);
+
+  const removeMouseover = useCallback((el: Selection<d3.BaseType, {}, null, undefined>) => {
+    return el.on("mousemove", null).on("mouseout", null);
+  },[])
+
+  useEffect(
+    () => {
+      if (mode === Mode.game) {
+        d3.selectAll('svg path')
+          .each(function(this: any) { removeMouseover(d3.select(this))});
+        resetBoard();
+      } else {
+        d3.selectAll('svg path')
+          .each(function(this: any, d: any) { applyMouseover(d3.select(this), d.properties.name)})
+      }
+    },
+    [applyMouseover, mode, removeMouseover]
+  );
+
+  useEffect(
+    () => {
+      if (!selected) {
+        return;
+      }
+
+      d3.selectAll('svg path').filter((d: any) => d.properties.name === selected)
+        .style('fill', '#FFEB3B');
+      return () => {
+        d3.selectAll('svg path').filter((d: any) => d.properties.name === selected)
+        .style('fill', function(d: any) {
+          if (allNeighbs.includes(d.properties.name)) {
+            return DEFAULT_FILL;
+          }
+          else if (missed.includes(d.properties.name)) {
+            return WRONG_FILL;
+          }
+          return CORRECT_FILL;
+        })}
+    },
+    [selected, allNeighbs, missed]
+  );
+
   useEffect(
     () => {
       if (!neighbToFind) {
@@ -44,34 +113,36 @@ export default function Map(): JSX.Element {
       }
 
       function eventHandler(this: any, d: any) {
-        if (d.properties.name === neighbToFind) {
+        if (d.properties.name !== selected) {
+          return setSelected(d.properties.name);
+        }
+        else if (d.properties.name === neighbToFind) {
           d3.selectAll('svg path').filter((dd: any) => d.properties.name === dd.properties.name)
-            .each(function(el){
+            .each(function(el: any){
               d3.select(this).style('fill', CORRECT_FILL).style('opacity', 1);
+              applyMouseover(d3.select(this), el.properties.name);
             })
         } else {
           d3.selectAll('svg path').filter((dd: any) => neighbToFind === dd.properties.name)
             .each(function(el: any){
-              d3.select(this).style('fill', WRONG_FILL).style('opacity', 1);
-              d3.select(this).insert('text')
-                .attr('dy', '0.5em')
-                .text(el.properties.name);
+              d3.select(this).style('fill', WRONG_FILL).style('opacity', 1)
+              applyMouseover(d3.select(this), el.properties.name);
             })
           setMissed((misses) => misses.concat(neighbToFind));
         }
-        console.log(this);
         if (svg.current) {
           svg.current.selectAll('path').on('mouseover', function() {
             d3.select(this).style('opacity', 0.5);
           });
         }
+        setSelected(null);
         nextNeighb();
       }
 
       d3.selectAll('path')
         .on('click', (d: any) => [...allNeighbs, neighbToFind].includes(d.properties.name) ? eventHandler(d) : null );
     },
-    [neighbToFind, nextNeighb, allNeighbs]
+    [neighbToFind, nextNeighb, allNeighbs, selected, tooltipRef, applyMouseover]
   )
 
   useLayoutEffect(
@@ -120,18 +191,38 @@ export default function Map(): JSX.Element {
     [rootRef]
   );
 
+  const GameMode = () => (
+    mode === Mode.game 
+    ? (
+      <Fragment>
+        <button type="button" onClick={startGame} id="start-game-btn">Start New Game</button>
+        { neighbToFind && <div>
+          <div><p>Can you find and click on {neighbToFind}?</p></div>
+          <div><button type="button" onClick={skip}>Skip and come back later</button></div>
+          </div>}
+        <aside style={{float: 'right'}}>
+
+          <h4>Missed Neighbs</h4>
+          <pre>{JSON.stringify(missed, null, 2)}</pre>
+        </aside>
+      </Fragment>
+    ) : (
+      <div/>
+    )
+  )
+
   return (
     <div style={{width: '900px', padding: '30px', margin: 'auto auto'}}>
-      <button type="button" onClick={startGame} id="start-game-btn">Start New Game</button>
-      { neighbToFind && <div>
-        <div><p>Can you find and click on {neighbToFind}?</p></div>
-        <div><button type="button" onClick={skip}>Skip and come back later</button></div>
-        </div>}
-      <aside style={{float: 'right'}}>
-        <h4>Missed Neighbs</h4>
-        <pre>{JSON.stringify(missed, null, 2)}</pre>
-      </aside>
+      <div>
+        <label htmlFor="game-mode">Learn Mode</label>
+        <input type="checkbox" name="game-mode" onChange={() => setMode(mode === Mode.game ? Mode.learn : Mode.game)} />
+      </div>
+      <div>
+        <GameMode />
+      </div>
       <div ref={rootRef}/>
+      <div id="tooltip" ref={tooltipRef} style={{position: 'absolute', 'zIndex': 10, visibility: 'hidden'}}></div>
+
     </div>
   );
 }
